e2e9202894deac78a2060f4c0369761bd5984efe
Marko Bauhardt <mb@101tec.com>
2008-07-18 06:44:12 +0000

    + add headers + delete the extracted indices and sequence files after merge process was successfully

--- src/main/java/net/sf/katta/index/indexer/merge/IndexMergeJob.java
+++ src/main/java/net/sf/katta/index/indexer/merge/IndexMergeJob.java
@@ -1,15 +1,39 @@
+/**
+ * Copyright 2008 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sf.katta.index.indexer.merge;
 
 import org.apache.hadoop.conf.Configurable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.mapred.JobConf;
+import net.sf.katta.util.Logger;
 
 public class IndexMergeJob implements Configurable {
 
   private Configuration _configuration;
 
   public void merge(Path kattaIndices) throws Exception {
+
+    Logger.info("collect all shards in this folder: " + kattaIndices);
+
     Path dedupPath = new Path("/tmp/katta.index.dedup", "" + System.currentTimeMillis());
 
     IndexToSequenceFileJob indexToSequenceFileJob = new IndexToSequenceFileJob();
@@ -19,6 +43,13 @@ public class IndexMergeJob implements Configurable {
     SequenceFileToIndexJob sequenceFileToIndexJob = new SequenceFileToIndexJob();
     sequenceFileToIndexJob.setConf(_configuration);
     sequenceFileToIndexJob.sequenceFileToIndex(dedupPath, kattaIndices);
+
+    Logger.info("delete sequence file and extracted indices: " + dedupPath);
+    FileSystem fileSystem = FileSystem.get(_configuration);
+    fileSystem.delete(dedupPath);
+
+    Logger.info("all shards in this folder are merged: " + kattaIndices);
+    Logger.info("move these shards into a archive folder");
   }
 
   public void setConf(Configuration configuration) {
--- src/main/java/net/sf/katta/index/indexer/merge/SequenceFileToIndexJob.java
+++ src/main/java/net/sf/katta/index/indexer/merge/SequenceFileToIndexJob.java
@@ -1,6 +1,28 @@
+/**
+ * Copyright 2008 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sf.katta.index.indexer.merge;
 
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import net.sf.katta.index.indexer.IndexJobConf;
 import net.sf.katta.index.indexer.ShardSelectionMapper;
@@ -17,6 +39,8 @@ import org.apache.hadoop.mapred.SequenceFileInputFormat;
 public class SequenceFileToIndexJob implements Configurable {
 
   private Configuration _configuration;
+
+  private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd.hhmmss");
 
   public void sequenceFileToIndex(Path sequenceFilePath, Path outputFolder) throws Exception {
 
@@ -42,13 +66,13 @@ public class SequenceFileToIndexJob implements Configurable {
     //the input key and input value class which is saved in the sequence file will be mapped out as value: BytesWritable
     jobConf.set("index.input.key.class", Text.class.getName());
     jobConf.set("index.input.value.class", DocumentInformation.class.getName());
-    String indexFolder = "" + System.currentTimeMillis() + "-merge";
+    String indexFolder = "mergedIndex-" + DATE_FORMAT.format(new Date());
 
     Path newOutputPath = new Path(jobConf.getOutputPath(), indexFolder);
     Logger.info("set mapred folder to: " + newOutputPath);
     jobConf.setOutputPath(newOutputPath);
 
-    String uploadPath = outputFolder.toString() +"/" + indexFolder;
+    String uploadPath = outputFolder.toString() + "/" + indexFolder;
     Logger.info("set index upload folder: '" + uploadPath + "'");
     jobConf.set(IndexJobConf.INDEX_UPLOAD_PATH, uploadPath);
 
