444e682df261fed055550412592d28b1c74f0dda
Johannes Zillmann <jz@101tec.com>
2008-10-06 08:33:27 +0000

    add missing license header

--- src/test/java/net/sf/katta/testutil/StoreAction.java
+++ src/test/java/net/sf/katta/testutil/StoreAction.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sf.katta.testutil;
 
 import java.util.ArrayList;
@@ -12,34 +27,34 @@ import org.jmock.lib.action.CustomAction;
  */
 public class StoreAction extends CustomAction {
 
-  private List<Object[]> _parameters = new ArrayList<Object[]>();
+	private List<Object[]> _parameters = new ArrayList<Object[]>();
 
-  private Action _actionToInvoke;
+	private Action _actionToInvoke;
 
-  public StoreAction() {
-    super("this action will store the parameters of all method calls");
-  }
+	public StoreAction() {
+		super("this action will store the parameters of all method calls");
+	}
 
-  public StoreAction(Action actionToInvoke) {
-    this();
-    _actionToInvoke = actionToInvoke;
-  }
+	public StoreAction(Action actionToInvoke) {
+		this();
+		_actionToInvoke = actionToInvoke;
+	}
 
-  public Object invoke(Invocation invocation) throws Throwable {
-    Object[] parametersAsArray = invocation.getParametersAsArray();
-    _parameters.add(parametersAsArray);
-    if (_actionToInvoke != null) {
-      return _actionToInvoke.invoke(invocation);
-    }
-    return null;
-  }
+	public Object invoke(Invocation invocation) throws Throwable {
+		Object[] parametersAsArray = invocation.getParametersAsArray();
+		_parameters.add(parametersAsArray);
+		if (_actionToInvoke != null) {
+			return _actionToInvoke.invoke(invocation);
+		}
+		return null;
+	}
 
-  public List<Object[]> getParameters() {
-    return _parameters;
-  }
+	public List<Object[]> getParameters() {
+		return _parameters;
+	}
 
-  public void reset() {
-    _parameters.clear();
-  }
+	public void reset() {
+		_parameters.clear();
+	}
 
 }
