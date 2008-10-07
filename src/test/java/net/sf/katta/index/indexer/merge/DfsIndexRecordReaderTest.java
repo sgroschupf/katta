7e5fe02aa8c6bf0de06b7b1e391cc7d123f4ef0c
Marko Bauhardt <mb@101tec.com>
2008-10-07 12:54:55 +0000

    fix test

--- src/test/java/net/sf/katta/index/indexer/merge/DfsIndexRecordReaderTest.java
+++ src/test/java/net/sf/katta/index/indexer/merge/DfsIndexRecordReaderTest.java
@@ -17,12 +17,14 @@ package net.sf.katta.index.indexer.merge;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 
 import net.sf.katta.testutil.ExtendedTestCase;
 
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.MD5Hash;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.FileSplit;
@@ -33,49 +35,42 @@ import org.jmock.Mockery;
 
 public class DfsIndexRecordReaderTest extends ExtendedTestCase {
 
-	private File _file = createFile("_temporary/jobId");
+  private File _file = createFile("_temporary/jobId");
 
-	public void testNext() throws IOException, URISyntaxException {
-		JobConf jobConf = new JobConf();
-		Path out = new Path(_file.getAbsolutePath());
-		FileOutputFormat.setOutputPath(jobConf, out);
+  public void testNext() throws IOException, URISyntaxException {
+    JobConf jobConf = new JobConf();
+    Path out = new Path(_file.getAbsolutePath());
+    FileOutputFormat.setOutputPath(jobConf, out);
 
-		Mockery mockery = new Mockery();
-		Path path = new Path("src/test/testIndexB/aIndex.zip");
-		FileSystem fileSystem = FileSystem.get(jobConf);
-		long len = fileSystem.getFileStatus(path).getLen();
-		FileSplit fileSplit = new FileSplit(path, 0, len, (String[]) null);
-		final IDocumentDuplicateInformation duplicateInformation = mockery
-				.mock(IDocumentDuplicateInformation.class);
+    Mockery mockery = new Mockery();
+    Path path = new Path("src/test/testIndexB/aIndex.zip");
+    FileSystem fileSystem = FileSystem.get(jobConf);
+    long len = fileSystem.getFileStatus(path).getLen();
+    FileSplit fileSplit = new FileSplit(path, 0, len, (String[]) null);
+    final IDocumentDuplicateInformation duplicateInformation = mockery.mock(IDocumentDuplicateInformation.class);
 
-		DfsIndexRecordReader reader = new DfsIndexRecordReader(jobConf,
-				fileSplit, duplicateInformation);
+    DfsIndexRecordReader reader = new DfsIndexRecordReader(jobConf, fileSplit, duplicateInformation);
 
-		mockery.checking(new Expectations() {
-			{
-				one(duplicateInformation).getSupportedFieldNames();
-				one(duplicateInformation).getKey(with(any(Document.class)));
-				will(returnValue("foo"));
-				one(duplicateInformation).getSortValue(
-						with(any(Document.class)));
-				will(returnValue("1"));
-			}
-		});
+    mockery.checking(new Expectations() {
+      {
+        one(duplicateInformation).getSupportedFieldNames();
+        one(duplicateInformation).getKey(with(any(Document.class)));
+        will(returnValue("foo"));
+        one(duplicateInformation).getSortValue(with(any(Document.class)));
+        will(returnValue("1"));
+      }
+    });
 
-		Text text = reader.createKey();
-		DocumentInformation information = reader.createValue();
-		reader.next(text, information);
+    Text text = reader.createKey();
+    DocumentInformation information = reader.createValue();
+    reader.next(text, information);
 
-		assertEquals("foo", text.toString());
-		assertEquals(0, information.getDocId().get());
-		// TODO jz: test is failing, please fix
-		// assertEquals(new File(out.getParent().getParent().toString(),
-		// ".indexes/" + path.getName() + "-"
-		// + MD5Hash.digest(path.toString()) + "-uncompress")
-		// .getAbsolutePath(), new File(new URI(information.getIndexPath()
-		// .toString())).getAbsolutePath());
-		assertEquals("1", information.getSortValue().toString());
+    assertEquals("foo", text.toString());
+    assertEquals(0, information.getDocId().get());
+    assertEquals(new File(out.toString(), ".indexes/" + path.getName() + "-" + MD5Hash.digest(path.toString())
+        + "-uncompress").getAbsolutePath(), new File(new URI(information.getIndexPath().toString())).getAbsolutePath());
+    assertEquals("1", information.getSortValue().toString());
 
-		mockery.assertIsSatisfied();
-	}
+    mockery.assertIsSatisfied();
+  }
 }
