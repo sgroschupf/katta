3a48f5c4a2822ebe91f4f7dd506b78cf8f10e478
Johannes Zillmann <jz@datameer.com>
2009-12-29 15:30:32 +0100

    KATTA-97 gracefull shutdown of JmxMonitor

--- src/main/java/net/sf/katta/node/monitor/JmxMonitor.java
+++ src/main/java/net/sf/katta/node/monitor/JmxMonitor.java
@@ -25,14 +25,16 @@ import java.util.List;
 
 import net.sf.katta.protocol.InteractionProtocol;
 
+import org.I0Itec.zkclient.ExceptionUtil;
 import org.apache.log4j.Logger;
 
 public class JmxMonitor implements IMonitor {
+
   protected final static Logger LOG = Logger.getLogger(JmxMonitor.class);
 
-  private InteractionProtocol _protocol;
-  private JmxMonitorThread _thread;
-  private String _serverId;
+  protected InteractionProtocol _protocol;
+  protected JmxMonitorThread _thread;
+  protected String _serverId;
 
   @Override
   public void startMonitoring(String serverId, InteractionProtocol protocol) {
@@ -48,80 +50,87 @@ public class JmxMonitor implements IMonitor {
   @Override
   public void stopMonitoring() {
     _thread.interrupt();
-    _thread.stop();// TODO KATTA-97 improve
+    try {
+      _thread.join();
+    } catch (InterruptedException e) {
+      ExceptionUtil.retainInterruptFlag(e);
+    }
   }
 
-  private class JmxMonitorThread extends Thread {
+  class JmxMonitorThread extends Thread {
 
     public void run() {
       // create the node for the first time...
-      while (!isInterrupted()) {
-        MetricsRecord metrics = new MetricsRecord(_serverId);
-        // there is no good way to get underlaying system matrics . Only
-        // com.sun.management.OperatingSystemMXBean provides some basic
-        // information. See:
-        // http://nadeausoftware.com/articles/2008/03/java%5Ftip%5Fhow%5Fget%5Fcpu%5Fand%5Fuser%5Ftime%5Fbenchmarking#UsingaSuninternalclasstogetJVMCPUtime
-
-        try {
-          // operation system
-          OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
-
-          Long processCpuTime = getValue(operatingSystemMXBean, "getProcessCpuTime");
-          metrics.addValue("ProcessCpuTime", processCpuTime, System.currentTimeMillis());
-
-          Long totalSwapSpaceSize = getValue(operatingSystemMXBean, "getTotalSwapSpaceSize");
-          metrics.addValue("TotalSwapSpaceSize", totalSwapSpaceSize, System.currentTimeMillis());
-          Long freeSwapSpaceSize = getValue(operatingSystemMXBean, "getFreeSwapSpaceSize");
-          metrics.addValue("FreeSwapSpaceSize", freeSwapSpaceSize, System.currentTimeMillis());
-
-          Long freePhysicalMemorySize = getValue(operatingSystemMXBean, "getFreePhysicalMemorySize");
-          metrics.addValue("FreePhysicalMemorySize", freePhysicalMemorySize, System.currentTimeMillis());
-          Long totalPhysicalMemorySize = getValue(operatingSystemMXBean, "getTotalPhysicalMemorySize");
-          metrics.addValue("TotalPhysicalMemorySize", totalPhysicalMemorySize, System.currentTimeMillis());
-
-          Long committedVirtualMemorySize = getValue(operatingSystemMXBean, "getCommittedVirtualMemorySize");
-          metrics.addValue("CommittedVirtualMemorySize", committedVirtualMemorySize, System.currentTimeMillis());
-
-          // memory
-          MemoryUsage heapMemoryUsage = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
-          metrics.addValue("heapMemoryUsageMax", heapMemoryUsage.getMax(), System.currentTimeMillis());
-          metrics.addValue("heapMemoryUsageUsed", heapMemoryUsage.getUsed(), System.currentTimeMillis());
-          metrics.addValue("heapMemoryUsageInit", heapMemoryUsage.getInit(), System.currentTimeMillis());
-          metrics.addValue("heapMemoryUsageCommited", heapMemoryUsage.getCommitted(), System.currentTimeMillis());
-
-          MemoryUsage nonHeapMemoryUsage = ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage();
-          metrics.addValue("nonHeapMemoryUsageMax", nonHeapMemoryUsage.getMax(), System.currentTimeMillis());
-          metrics.addValue("nonHeapMemoryUsageUsed", nonHeapMemoryUsage.getUsed(), System.currentTimeMillis());
-          metrics.addValue("nonHeapMemoryUsageInit", nonHeapMemoryUsage.getInit(), System.currentTimeMillis());
-          metrics.addValue("nonHeapMemoryUsageCommited", nonHeapMemoryUsage.getCommitted(), System.currentTimeMillis());
-
-          // Garbage collection
-          List<GarbageCollectorMXBean> garbageCollectorMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
-          for (GarbageCollectorMXBean bean : garbageCollectorMXBeans) {
-            String gcName = bean.getName();
-            long collectionCount = bean.getCollectionCount();
-            metrics.addValue("collectionCount_" + gcName, collectionCount, System.currentTimeMillis());
-            long collectionTime = bean.getCollectionTime();
-            metrics.addValue("collectionTime_" + gcName, collectionTime, System.currentTimeMillis());
-          }
-          // File System
-          // TODO sg 10/03/09 We should add diskfree as well, but we need to
-          // know which
-          // disks we talk about. This brings in the problem that currently we
-          // can only use one not multiple disks for the shards. So lets fix
-          // this as soon we fix this other problem :/
-
-        } catch (Exception e) {
-          LOG.error("Unable to retrieve metrics values:", e);
-        }
-        _protocol.setMetric(_serverId, metrics);
-        try {
+      try {
+        while (true) {
+          MetricsRecord metrics = new MetricsRecord(_serverId);
+          gatherMetrics(metrics);
+          _protocol.setMetric(_serverId, metrics);
           sleep(1000);
-        } catch (InterruptedException e) {
-          LOG.debug("Sleep was interrupted", e);
         }
+      } catch (InterruptedException e) {
+        ExceptionUtil.retainInterruptFlag(e);
       }
       LOG.info("jmx monitor stopped");
+    }
+
+    private void gatherMetrics(MetricsRecord metrics) {
+      // there is no good way to get underlaying system matrics . Only
+      // com.sun.management.OperatingSystemMXBean provides some basic
+      // information. See:
+      // http://nadeausoftware.com/articles/2008/03/java%5Ftip%5Fhow%5Fget%5Fcpu%5Fand%5Fuser%5Ftime%5Fbenchmarking#UsingaSuninternalclasstogetJVMCPUtime
+      try {
+        // operation system
+        OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+
+        Long processCpuTime = getValue(operatingSystemMXBean, "getProcessCpuTime");
+        metrics.addValue("ProcessCpuTime", processCpuTime, System.currentTimeMillis());
+
+        Long totalSwapSpaceSize = getValue(operatingSystemMXBean, "getTotalSwapSpaceSize");
+        metrics.addValue("TotalSwapSpaceSize", totalSwapSpaceSize, System.currentTimeMillis());
+        Long freeSwapSpaceSize = getValue(operatingSystemMXBean, "getFreeSwapSpaceSize");
+        metrics.addValue("FreeSwapSpaceSize", freeSwapSpaceSize, System.currentTimeMillis());
+
+        Long freePhysicalMemorySize = getValue(operatingSystemMXBean, "getFreePhysicalMemorySize");
+        metrics.addValue("FreePhysicalMemorySize", freePhysicalMemorySize, System.currentTimeMillis());
+        Long totalPhysicalMemorySize = getValue(operatingSystemMXBean, "getTotalPhysicalMemorySize");
+        metrics.addValue("TotalPhysicalMemorySize", totalPhysicalMemorySize, System.currentTimeMillis());
+
+        Long committedVirtualMemorySize = getValue(operatingSystemMXBean, "getCommittedVirtualMemorySize");
+        metrics.addValue("CommittedVirtualMemorySize", committedVirtualMemorySize, System.currentTimeMillis());
+
+        // memory
+        MemoryUsage heapMemoryUsage = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
+        metrics.addValue("heapMemoryUsageMax", heapMemoryUsage.getMax(), System.currentTimeMillis());
+        metrics.addValue("heapMemoryUsageUsed", heapMemoryUsage.getUsed(), System.currentTimeMillis());
+        metrics.addValue("heapMemoryUsageInit", heapMemoryUsage.getInit(), System.currentTimeMillis());
+        metrics.addValue("heapMemoryUsageCommited", heapMemoryUsage.getCommitted(), System.currentTimeMillis());
+
+        MemoryUsage nonHeapMemoryUsage = ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage();
+        metrics.addValue("nonHeapMemoryUsageMax", nonHeapMemoryUsage.getMax(), System.currentTimeMillis());
+        metrics.addValue("nonHeapMemoryUsageUsed", nonHeapMemoryUsage.getUsed(), System.currentTimeMillis());
+        metrics.addValue("nonHeapMemoryUsageInit", nonHeapMemoryUsage.getInit(), System.currentTimeMillis());
+        metrics.addValue("nonHeapMemoryUsageCommited", nonHeapMemoryUsage.getCommitted(), System.currentTimeMillis());
+
+        // Garbage collection
+        List<GarbageCollectorMXBean> garbageCollectorMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean bean : garbageCollectorMXBeans) {
+          String gcName = bean.getName();
+          long collectionCount = bean.getCollectionCount();
+          metrics.addValue("collectionCount_" + gcName, collectionCount, System.currentTimeMillis());
+          long collectionTime = bean.getCollectionTime();
+          metrics.addValue("collectionTime_" + gcName, collectionTime, System.currentTimeMillis());
+        }
+        // File System
+        // TODO sg 10/03/09 We should add diskfree as well, but we need to
+        // know which
+        // disks we talk about. This brings in the problem that currently we
+        // can only use one not multiple disks for the shards. So lets fix
+        // this as soon we fix this other problem :/
+
+      } catch (Exception e) {
+        LOG.error("Unable to retrieve metrics values:", e);
+      }
     }
 
     private Long getValue(OperatingSystemMXBean operatingSystemMXBean, String methodName) throws NoSuchMethodException,
