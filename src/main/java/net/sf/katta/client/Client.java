969a790ea91c64518e3de7c33aa0a5cc6ed30d98
Johannes Zillmann <jz@datameer.com>
2010-08-02 17:13:00 +0200

    KATTA-146, fix ConcurrentModificationException when multiple LuceneClient objects are created simultaneously

--- src/main/java/net/sf/katta/client/Client.java
+++ src/main/java/net/sf/katta/client/Client.java
@@ -97,9 +97,12 @@ public class Client implements IShardProxyManager, ConnectedComponent {
   public Client(Class<? extends VersionedProtocol> serverClass, final INodeSelectionPolicy policy,
           final InteractionProtocol protocol, ClientConfiguration clientConfiguration) {
     Set<String> keys = new HashSet<String>(clientConfiguration.getKeys());
-    for (String key : keys) {
-      // simply set all properties / adding non-hadoop properties shouldn't hurt
-      _hadoopConf.set(key, clientConfiguration.getProperty(key));
+    synchronized (Configuration.class) {// fix for KATTA-146
+      for (String key : keys) {
+        // simply set all properties / adding non-hadoop properties shouldn't
+        // hurt
+        _hadoopConf.set(key, clientConfiguration.getProperty(key));
+      }
     }
 
     _serverClass = serverClass;
